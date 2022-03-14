55a665238da1be1671cb8b211b0e9ba440e7cf47
SmaugADG <summerskyadg@163.com>
2022-03-14 15:15:47 +0800

    外星人入侵--v1

---
+++ alien_invasion/settings.py
@@ -0,0 +1,19 @@
+class Settings:
+    '''存储所有设置'''
+    def __init__(self):
+        '''初始化游戏设置'''
+        # 屏幕设置
+        self.screen_width=800
+        self.screen_height=900
+        self.bg_color=(230,230,230)
+
+        # 子弹设置
+        self.bullet_speed=1.0
+        self.bullet_width=3
+        self.bullet_height=15
+        self.bullet_color=(60,60,60)
+        self.bullet_allowed=3
+
+
+        # 飞船设置
+        self.ship_speed=1.5
---
+++ alien_invasion/test.py
@@ -0,0 +1,5 @@
+from alion_invasion import AlienInvasion
+
+game=AlienInvasion()
+game.__init__()
+game.run_game()
